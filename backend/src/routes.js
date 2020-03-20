9dc955d6bae8b85acce433ed9510588e7ec27b74
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-03-19 23:59:08 -0300

    Criação do controller TypeMember

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 // import authMiddlewares from './app/middlewares/auth';
+import TypeMemberController from './app/controllers/TypeMemberController';
 
 const routes = new Router();
 
@@ -9,5 +10,10 @@ const routes = new Router();
 routes.get('/', (req, res) => {
     return res.json({ message: 'Hello Word!' });
 });
+
+routes.post('/type-members', TypeMemberController.store);
+routes.get('/type-members', TypeMemberController.index);
+routes.put('/type-members', TypeMemberController.update);
+routes.delete('/type-members', TypeMemberController.delete);
 
 export default routes;
