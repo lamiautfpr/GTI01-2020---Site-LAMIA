927d8ec16c308128ff170362b1421570cc196c7a
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-03-22 02:53:12 -0300

    Definindo altenticação para as rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,24 +2,36 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-// import authMiddlewares from './app/middlewares/auth';
+import authMiddlewares from './app/middlewares/auth';
+import SessionController from './app/controllers/SessionController';
 import TypeMemberController from './app/controllers/TypeMemberController';
 import FileController from './app/controllers/FileController';
+import MemberController from './app/controllers/MemberController';
+import OfficeController from './app/controllers/OfficeController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
-
-// routes.use(authMiddlewares);
 
 routes.get('/', (req, res) => {
     return res.json({ msessage: 'Hello Word!' });
 });
 
-routes.post('/type-members', TypeMemberController.store);
 routes.get('/type-members', TypeMemberController.index);
+routes.get('/members', MemberController.index);
+routes.get('/members/:office', OfficeController.index);
+
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddlewares);
+
+routes.post('/type-members', TypeMemberController.store);
+
 routes.put('/type-members', TypeMemberController.update);
 routes.delete('/type-members', TypeMemberController.delete);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/members', MemberController.store);
+routes.put('/members', MemberController.update);
 
 export default routes;
