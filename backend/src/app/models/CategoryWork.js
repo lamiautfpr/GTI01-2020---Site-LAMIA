36ddcbe50a375b10d5c017e593baf6e088d10300
Rafael Lechensque <rafaelaquino@alunos.utfpr.edu.br>
2020-04-08 18:12:30 -0300

    Criação migration, model e controller de CategoryWork

---
+++ backend/src/app/models/CategoryWork.js
@@ -0,0 +1,33 @@
+import Sequelize, { Model } from 'sequelize';
+
+class CategoryWork extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                name: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                    validate: {
+                        notNull: true,
+                        notEmpty: true,
+                    },
+                },
+                description: {
+                    type: Sequelize.TEXT,
+                    allowNull: true,
+                    validate: {
+                        notNull: false,
+                        notEmpty: true,
+                    },
+                },
+            },
+            {
+                sequelize,
+            }
+        );
+
+        return this;
+    }
+}
+
+export default CategoryWork;
---
+++ backend/src/database/migrations/20200408143026-create-categoty_works.js
@@ -0,0 +1,32 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('categoty_works', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            description: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: queryInterface => {
+        return queryInterface.dropTable('categoty_works');
+    },
+};
