4e8ab12d3870e5a2eaa56f41db721e0e33456122
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-06-02 20:00:34 -0300

    Atualização de indentação

--- backend/src/app/models/Picture.js
+++ backend/src/app/models/Picture.js
@@ -1,47 +1,47 @@
 import Sequelize, { Model } from 'sequelize';
 
 class Picture extends Model {
-    static init(sequelize) {
-        super.init(
-            {
-                name: {
-                    type: Sequelize.STRING,
-                    allowNull: false,
-                    validate: {
-                        notNull: true,
-                        notEmpty: true,
-                    },
-                },
-                path: {
-                    type: Sequelize.TEXT,
-                    allowNull: true,
-                    validate: {
-                        notNull: false,
-                        notEmpty: false,
-                    },
-                },
-                url: {
-                    type: Sequelize.VIRTUAL,
-                    get() {
-                        return `${process.env.APP_URL}/files/${this.path}`;
-                    },
-                },
-            },
-            {
-                sequelize,
-            }
-        );
+  static init(sequelize) {
+    super.init(
+      {
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          validate: {
+            notNull: true,
+            notEmpty: true,
+          },
+        },
+        path: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+          validate: {
+            notNull: false,
+            notEmpty: false,
+          },
+        },
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `${process.env.APP_URL}/files/${this.path}`;
+          },
+        },
+      },
+      {
+        sequelize,
+      }
+    );
 
-        return this;
-    }
+    return this;
+  }
 
-    static associate(models) {
-        this.belongsToMany(models.Work, {
-            foreignKey: 'picture_id	',
-            as: 'works',
-            through: 'pictures_works',
-        });
-    }
+  static associate(models) {
+    this.belongsToMany(models.Work, {
+      foreignKey: 'picture_id	',
+      as: 'works',
+      through: 'pictures_works',
+    });
+  }
 }
 
 export default Picture;
