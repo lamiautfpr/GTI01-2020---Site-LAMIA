4e1c21984aecf8041e29670dacb7ab840d820ab8
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-06-03 19:17:47 -0300

    Add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,28 +2,30 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import routes from './routes';
 
 import './database';
 
 class App {
-    constructor() {
-        this.server = express();
-        this.middlewares();
-        this.routes();
-    }
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
 
-    middlewares() {
-        this.server.use(express.json());
-        this.server.use(
-            '/files',
-            express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
-        );
-    }
+  middlewares() {
+    this.server.use(cors());
+    this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
+  }
 
-    routes() {
-        this.server.use(routes);
-    }
+  routes() {
+    this.server.use(routes);
+  }
 }
 
 export default new App().server;
