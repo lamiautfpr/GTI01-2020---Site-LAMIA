84f8f4cd3c49bc2e062de12a57a0a0eec38164f8
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-07-22 17:37:41 -0300

    Creating Tooltip

---
+++ frontend/src/components/Tooltip/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+interface ITooltipProps {
+  title: string;
+  className?: string;
+}
+
+const Tooltip: React.FC<ITooltipProps> = ({
+  title,
+  className = '',
+  children,
+}) => {
+  return (
+    <Container className={className}>
+      {children}
+      <span>{title}</span>
+    </Container>
+  );
+};
+
+export default Tooltip;
