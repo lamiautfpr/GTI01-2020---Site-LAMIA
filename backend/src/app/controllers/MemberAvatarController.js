58a5684d0ad88479e17674226bc22e1e384a5337
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-08-04 16:19:30 -0300

    update MemberAvatarController

--- backend/src/app/controllers/MemberAvatarController.js
+++ backend/src/app/controllers/MemberAvatarController.js
@@ -3,10 +3,24 @@ import fs from 'fs';
 import uploadConfig from '../../config/multer';
 import Picture from '../models/Picture';
 import Member from '../models/Member';
+import TypeMember from '../models/TypeMember';
 
 class MemberAvatarController {
   async update(req, res) {
-    const member = await Member.findByPk(req.userId);
+    const member = await Member.findByPk(req.userId, {
+      include: [
+        {
+          model: TypeMember,
+          as: 'office',
+          attributes: ['name', 'label', 'id', 'value'],
+        },
+        {
+          model: Picture,
+          as: 'avatar',
+          attributes: ['name', 'path', 'src'],
+        },
+      ],
+    });
     const { originalname: name, filename: path } = req.file;
 
     if (!member) {
@@ -14,17 +28,23 @@ class MemberAvatarController {
     }
     if (!member.avatar_id) {
       const picture = await Picture.create({ name, path });
-      console.log(member.avatar_id);
-      return res.json(picture);
+      member.update({ avatar_id: picture.id, avatar: picture });
+
+      return res.json(member);
     }
+
     const picture = await Picture.findByPk(member.avatar_id);
     const filePath = resolve(uploadConfig.folderUpload, picture.path);
 
-    await fs.promises.unlink(filePath);
+    try {
+      await fs.promises.unlink(filePath);
+      // eslint-disable-next-line no-empty
+    } catch (err) {}
 
     await picture.update({ name, path });
+    member.update({ avatar_id: picture.id, avatar: picture });
 
-    return res.json(picture);
+    return res.json(member);
   }
 }
 
