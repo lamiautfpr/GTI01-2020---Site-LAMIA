35882ec31d3f50c9b7ebb29af61b8f1e958e88d7
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-11-24 15:46:48 -0300

    T43-Criação das migrations

---
+++ src/database/migrations/20201124175836-create-news.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('news', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      cover: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      date_publication: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('news');
+  },
+};
---
+++ src/database/migrations/20201124175836-create-pictures_news.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('pictures_news', {
+      picture_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'pictures', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: true,
+      },
+      new_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'news', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('pictures_news');
+  },
+};
