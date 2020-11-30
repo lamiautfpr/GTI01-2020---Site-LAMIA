f05d1b03731eaafbbff664c4c1b9d647a4d84343
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-11-30 16:27:10 -0300

    T56-Atualizando database config para env

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'postgres',
-  host: '',
-  username: '',
-  password: '',
-  database: '',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
