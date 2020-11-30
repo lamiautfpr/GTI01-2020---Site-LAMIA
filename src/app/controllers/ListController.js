8506dc106300124ad734ebd2290a7ffa3899dd35
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-11-30 15:19:06 -0300

    T56-Correção da listagem de works

--- src/app/controllers/ListController.js
+++ src/app/controllers/ListController.js
@@ -36,7 +36,7 @@ class ListController {
         {
           model: Work,
           as: 'works',
-          attributes: ['id', 'title', 'date_begin', 'dateBegin'],
+          attributes: ['id', 'title', 'date_begin', 'dateBegin', 'objective'],
           include: [
             {
               model: Picture,
