01aecacf385ad91e1a83b6af874415a738c8886f
Jece Xavier <jece.pereira@etec.sp.gov.br>
2021-04-25 11:49:39 -0300

    removing example module

--- new-api/src/v2/modules/_example/dtos/IFindExample.dto.ts
+++
@@ -1,6 +0,0 @@
-export interface IFindExampleDTO {
-  id?: string;
-  name?: string;
-  createAt?: Date;
-  updateAt?: Date;
-}
--- new-api/src/v2/modules/_example/dtos/IOrderExample.dto.ts
+++
@@ -1,5 +0,0 @@
-import { ExampleEntity } from '../typeorm/example.entity';
-
-export type IOrderExampleDTO = {
-  [K in keyof ExampleEntity]?: 'ASC' | 'DESC';
-};
