23e3084182b4abbe03eafa4e358bcfbb767d6be7
zM4l0y <joaovinipach@gmail.com>
2021-09-15 13:58:51 -0300

    create migrations create-table-category and insert-categories

---
+++ new-api/src/v2/migrations/1631722522030-create-table-category.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class insertCategories1631722522030 implements MigrationInterface {
+  name = 'insertCategories1631722522030';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "tb_category" ("id" character varying NOT NULL, "createAt" TIMESTAMP NOT NULL DEFAULT now(), "updateAt" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying NOT NULL, "description" character varying, CONSTRAINT "PK_394d39970459679271183ff2bee" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "tb_category"`);
+  }
+}
