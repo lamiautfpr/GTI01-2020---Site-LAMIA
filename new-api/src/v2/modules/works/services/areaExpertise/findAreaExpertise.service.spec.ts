39114f3d87e063fb9dee3475185d79df427c6954
Gustavo Silva Quieregato <quieregatog@alunos.utfpr.edu.br>
2021-09-19 08:11:52 -0300

    create to test findAreaExpertise

--- new-api/src/v2/modules/works/services/areaExpertise/findAreaExpertise.service.spec.ts
+++ new-api/src/v2/modules/works/services/areaExpertise/findAreaExpertise.service.spec.ts
@@ -1,36 +1,59 @@
+import { FakeRepositoryMember } from '@modules/members/repositories/fakes/Member.fakeRepository';
+import { FakeRepositoryPatent } from '@modules/members/repositories/fakes/Patent.fakeRepository';
+import { ServiceMember } from '@modules/members/services/member.service';
 import { FakeRepositoryAreaExpertise } from '@modules/works/repositories/fakes/AreaExpertise.fakeRepository';
-import {
-  ConflictException,
-  ForbiddenException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { ApiNoContentResponse } from '@nestjs/swagger';
+import IHashProvider from '@providers/HashProvider/models/IHashProvider';
+import IStorageProvider from '@providers/StorageProvider/models/IStorageProvider';
 import { ServiceAreaExpertise } from '../areaExpertise.service';
 
-
+let iHashProvider: IHashProvider;
+let iStorageProver: IStorageProvider;
+let fakeRepositoryMember: FakeRepositoryMember;
+let fakeRepositoryPatent: FakeRepositoryPatent;
 let fakeRepositoryAreaExpertise: FakeRepositoryAreaExpertise;
 let serviceAreaExpertise: ServiceAreaExpertise;
+let serviceMember: ServiceMember;
 
-describe('Find to Test - Services', () => {
+describe('Create Area Expertise - Service', () => {
   beforeEach(() => {
     fakeRepositoryAreaExpertise = new FakeRepositoryAreaExpertise();
+    fakeRepositoryMember = new FakeRepositoryMember();
+    fakeRepositoryPatent = new FakeRepositoryPatent();
+
+    serviceMember = new ServiceMember(
+      fakeRepositoryMember,
+      fakeRepositoryPatent,
+      iHashProvider,
+      iStorageProver,
+    );
+
     serviceAreaExpertise = new ServiceAreaExpertise(
       fakeRepositoryAreaExpertise,
+      serviceMember,
     );
   });
 
-  describe('successful case of areaExpertise', () => {
-    it('should list on areaExpertise exists', async () => {
+  describe('successful cases', () => {
+    it('should return a list of areaExpertise when Expertise exist', async () => {
       await fakeRepositoryAreaExpertise.createSave({
-        
-      })
+        name: 'Ciência de Dados',
+      });
+      await fakeRepositoryAreaExpertise.createSave({
+        name: 'Inteligência Artificial',
+      });
 
       const result = await serviceAreaExpertise.findAll({});
 
-      expect(result.length).toBe(0);
-      // expect(result[0].name).toBe('AreaExpertise 1');
-      // expect(result[1].name).toBe('AreaExpertise 2');
+      expect(result.length).toBe(2);
+      expect(result[0].name).toBe('Ciência de Dados');
+      expect(result[1].name).toBe('Inteligência Artificial');
+    });
 
-    })
-  })
-})
-
+    it('should return to NO_CONTENT when no areaExpertise exists', async () => {
+      await expect(serviceAreaExpertise.findAll({})).rejects.toBeInstanceOf(
+        ApiNoContentResponse,
+      );
+    });
+  });
+});
