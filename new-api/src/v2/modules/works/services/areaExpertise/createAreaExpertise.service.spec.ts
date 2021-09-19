2dd253622211161c3acd90f48ac4929a9bf05d98
Gustavo Silva Quieregato <quieregatog@alunos.utfpr.edu.br>
2021-09-19 16:49:29 -0300

    Error test to new Provider storage in createAreaExpertise.service.spec

--- new-api/src/v2/modules/works/services/areaExpertise/createAreaExpertise.service.spec.ts
+++ new-api/src/v2/modules/works/services/areaExpertise/createAreaExpertise.service.spec.ts
@@ -1,23 +1,54 @@
+import { FakeRepositoryMember } from '@modules/members/repositories/fakes/Member.fakeRepository';
+import { FakeRepositoryPatent } from '@modules/members/repositories/fakes/Patent.fakeRepository';
+import { ServiceMember } from '@modules/members/services/member.service';
 import { FakeRepositoryAreaExpertise } from '@modules/works/repositories/fakes/AreaExpertise.fakeRepository';
+import { EntityAreaExpertise } from '@modules/works/typeorm/entities/areaExpertise.entity';
 import { ConflictException } from '@nestjs/common';
+import IHashProvider from '@providers/HashProvider/models/IHashProvider';
+import IStorageProvider from '@providers/StorageProvider/models/IStorageProvider';
 import { ServiceAreaExpertise } from '../areaExpertise.service';
 
 let fakeRepositoryAreaExpertise: FakeRepositoryAreaExpertise;
+let fakeRepositoryMember: FakeRepositoryMember;
+let fakeRepositoryPatent: FakeRepositoryPatent;
 let serviceAreaExpertise: ServiceAreaExpertise;
+let serviceMember: ServiceMember;
+let iHashProvider: IHashProvider;
+let iStorageProver: IStorageProvider;
 
 describe('Create Area Expertise - SERVICES', () => {
   beforeEach(() => {
     fakeRepositoryAreaExpertise = new FakeRepositoryAreaExpertise();
+    fakeRepositoryMember = new FakeRepositoryMember();
+    fakeRepositoryPatent = new FakeRepositoryPatent();
+
+    serviceMember = new ServiceMember(
+      fakeRepositoryMember,
+      fakeRepositoryPatent,
+      iHashProvider,
+      iStorageProver,
+    );
+
     serviceAreaExpertise = new ServiceAreaExpertise(
       fakeRepositoryAreaExpertise,
+      serviceMember,
     );
   });
 
   describe('successful cases', () => {
     it('should create an area expertise successfully when there is full datas', async () => {
+      const idMember = '25ff2e6b-a777-41dc-827c-3fb8d6b4dbe7';
+
+      const users = serviceMember.findAll({});
+
+      const areaExpertise = new EntityAreaExpertise({
+        name: 'Ciência de dados',
+        description: 'Ciência de dados é uma area da computação que estuda...',
+      });
+
       const result = await serviceAreaExpertise.createAreaExpertise({
-        name: 'Area Expertise MocK',
-        description: 'this is description',
+        areaExpertise,
+        idMember,
       });
 
       expect(result).toHaveProperty('id');
@@ -27,8 +58,17 @@ describe('Create Area Expertise - SERVICES', () => {
     });
 
     it('should create an area expertise successfully when there not is description', async () => {
+      const idMember = '25ff2e6b-a777-41dc-827c-3fb8d6b4dbe7';
+
+      const users = serviceMember.findAll({});
+
+      const areaExpertise = new EntityAreaExpertise({
+        name: 'Inteligência Artificial',
+      });
+
       const result = await serviceAreaExpertise.createAreaExpertise({
-        name: 'Area Expertise MocK',
+        areaExpertise,
+        idMember,
       });
 
       expect(result).toHaveProperty('id');
@@ -39,27 +79,19 @@ describe('Create Area Expertise - SERVICES', () => {
   });
   describe('failure cases', () => {
     it('should not create an area expertise when already exists one with the same name and without description', async () => {
-      await serviceAreaExpertise.createAreaExpertise({
-        name: 'Area Expertise MocK',
+      const idMember = '25ff2e6b-a777-41dc-827c-3fb8d6b4dbe7';
+
+      const users = serviceMember.findAll({});
+
+      const areaExpertise = new EntityAreaExpertise({
+        name: 'Ciência de dados',
+        description: '',
       });
 
       expect(
         serviceAreaExpertise.createAreaExpertise({
-          name: 'Area Expertise MocK',
-        }),
-      ).rejects.toBeInstanceOf(ConflictException);
-    });
-
-    it('should not create an area expertise when already exists one with the same name', async () => {
-      await serviceAreaExpertise.createAreaExpertise({
-        name: 'Area Expertise MocK',
-        description: 'this is a description',
-      });
-
-      expect(
-        serviceAreaExpertise.createAreaExpertise({
-          name: 'Area Expertise MocK',
-          description: 'this is a description',
+          areaExpertise,
+          idMember,
         }),
       ).rejects.toBeInstanceOf(ConflictException);
     });
