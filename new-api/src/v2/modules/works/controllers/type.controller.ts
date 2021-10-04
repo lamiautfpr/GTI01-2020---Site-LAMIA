47cfc1447f13ab8d276bd4d803f62b03ab938280
Rafael Lechensque <rafaelaquino@alunos.utfpr.edu.br>
2021-10-04 09:59:08 -0300

    feat: update controller types

--- new-api/src/v2/modules/works/controllers/type.controller.ts
+++ new-api/src/v2/modules/works/controllers/type.controller.ts
@@ -1,21 +1,21 @@
+import { JwtAuthGuard } from '@modules/members/guard/jwtAuth.guard';
 import {
   ApiBadRequestResponse,
   ApiConflictResponse,
   ApiCreatedResponse,
   ApiInternalServerErrorResponse,
-  ApiQuery,
-  ApiResponse,
   ApiTags,
-  ApiNoContentResponse,
+  ApiUnauthorizedResponse,
+  ApiForbiddenResponse,
+  ApiBearerAuth,
 } from '@nestjs/swagger';
 import {
   Body,
   Controller,
-  Get,
   Post,
-  Query,
   UsePipes,
   ValidationPipe,
+  UseGuards,
 } from '@nestjs/common';
 
 // Service
@@ -27,45 +27,27 @@ import { EntityType } from '../typeorm/entities/type.entity';
 // Erros
 import Errors from 'v2/utils/Errors';
 import ICreateTypeDTO from '../dtos/type/ICreateType.dto';
-import { ISelectOrderTypeDTO } from '../dtos/type/IOrderType.dto';
 import { apiConfig } from '@config/api';
 
 @ApiTags('Types')
 @Controller(`${apiConfig.version}/works/types`)
-export class ControllerAreaExpertise {
+export class ControllerType {
   constructor(private readonly serviceType: ServiceType) {}
 
   @ApiCreatedResponse({
     description: 'Created Success',
     type: EntityType,
   })
-  //Tira duvidas
   @ApiBadRequestResponse(Errors.BadRequest)
   @ApiConflictResponse(Errors.Conflict)
   @ApiInternalServerErrorResponse(Errors.InternalServer)
+  @ApiUnauthorizedResponse(Errors.Unauthorized)
+  @ApiForbiddenResponse(Errors.Forbidden)
+  @UseGuards(JwtAuthGuard)
   @UsePipes(new ValidationPipe())
+  @ApiBearerAuth()
   @Post()
   create(@Body() data: ICreateTypeDTO) {
     return this.serviceType.createType(data);
-  }
-
-  @ApiQuery({
-    type: ISelectOrderTypeDTO,
-  })
-  @ApiNoContentResponse({
-    description: 'Type not exists',
-  })
-  @ApiResponse({
-    status: 200,
-    description: 'List of Type(s)',
-    type: EntityType,
-    isArray: true,
-  })
-  @ApiBadRequestResponse(Errors.BadRequest)
-  @ApiInternalServerErrorResponse(Errors.InternalServer)
-  @UsePipes(new ValidationPipe())
-  @Get()
-  findAll(@Query() order: ISelectOrderTypeDTO) {
-    return this.serviceType.findAll(order);
   }
 }
