f4e5750005c03389aba405a296ab407786ca08de
Jece Xavier <jece.pereira@etec.sp.gov.br>
2021-12-01 09:52:09 -0300

    feat: adding handleError Conflict

--- new-api/src/v2/utils/Errors/HandleErrorORM/TypeOrmError.ts
+++ new-api/src/v2/utils/Errors/HandleErrorORM/TypeOrmError.ts
@@ -4,14 +4,22 @@ import IHandleOrmError from './IHandleOrmError';
 export default class TypeOrmError implements IHandleOrmError {
   private errors = ['QueryFailedError'];
 
+  // Documentation: https://www.postgresql.org/docs/10/errcodes-appendix.html POSTGRESQL ERROR CODES
   private error = {
     23503: (detail: string) => {
       const className = detail.split('tb_')[1].replace('".', '');
       const item = detail.split('=(')[1].split(')')[0];
 
       return {
-        error: `Not Found "${className}" with idddd: "${item}"`,
+        error: `NOT_FOUND "${className}" with id: "${item}".`,
         statusCode: 404,
+      };
+    },
+    23505: (detail: string) => {
+      const key = detail.replace(/\"/g, '').split('(')[1].split(')')[0];
+      return {
+        error: `CONFLICT "${key}" already exists.`,
+        statusCode: 409,
       };
     },
   };
@@ -21,6 +29,7 @@ export default class TypeOrmError implements IHandleOrmError {
   }
 
   handleError(exception: any): HttpExceptionResponse {
+    console.log(exception);
     if (Object.keys(this.error).includes(exception.code)) {
       return this.error[exception.code](exception.detail);
     }
