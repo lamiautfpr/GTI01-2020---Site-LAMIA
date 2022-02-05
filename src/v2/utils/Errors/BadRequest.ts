59586ef337f40507a763aecdacf9d771ec803eba
Jece Xavier <jece.pereira@etec.sp.gov.br>
2022-02-05 13:33:01 -0300

    refactor: updating BadRequest error class

--- src/v2/utils/Errors/BadRequest.ts
+++ src/v2/utils/Errors/BadRequest.ts
@@ -1,27 +1,59 @@
-import { ApiProperty, ApiResponseOptions } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
+
+export enum ERRORS_BAD_REQUEST {
+  BAD_REQUEST = 'BAD_REQUEST',
+}
 
 export class BadRequest {
   @ApiProperty({
-    default: 400,
-    description: 'HTTP Status Code',
+    type: 'string',
+    description: 'Date and time the request was made',
+    example: '2022-01-24T23:34:16.687Z',
   })
-  statusCode: 400;
+  timeStamp: string;
 
   @ApiProperty({
-    example: ['attributes must be a string'],
-    description: ' Error messages',
-    isArray: true,
+    type: 'string',
+    description: 'Path the request was made',
   })
-  message: string[];
+  path: string;
 
   @ApiProperty({
-    default: 'Bad Request',
-    description: 'Type Error',
+    type: 'string',
+    description: 'HTTP status code number',
+    default: '400',
   })
-  error: 'Bad Request';
+  statusCode: number;
+
+  @ApiProperty({
+    type: 'string',
+    description: 'HTTP status code name',
+    default: 'BAD_REQUEST',
+  })
+  errorMessage: string;
+
+  @ApiProperty({
+    type: 'string',
+    description: 'Method the request was made',
+    enum: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  })
+  method: string;
+
+  @ApiProperty({
+    type: 'string',
+    description: 'List of the attributes on payload that caused the error',
+    example: [
+      'name should not be null or undefined',
+      'name should not be empty',
+      'name must be a string',
+      'email must be an email',
+      'patentId must be a UUID',
+    ],
+  })
+  errors: string | string[];
 }
 
 export default {
-  description: 'Bad Request',
+  description: 'BAD_REQUEST',
   type: BadRequest,
-} as ApiResponseOptions;
+};
