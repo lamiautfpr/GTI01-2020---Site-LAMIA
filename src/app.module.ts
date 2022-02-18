e5412373a5ba38e1177c87eaa10a87d6bee0e342
Jece Xavier <jece.pereira@etec.sp.gov.br>
2022-02-18 03:34:29 -0300

    feat: creating route to health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { V2Module } from './v2/V2.module';
+import { TerminusModule } from '@nestjs/terminus';
+// import { HealthController } from './health/health.controller';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), V2Module],
+  imports: [ConfigModule.forRoot(), TerminusModule, HealthModule, V2Module],
+  // controllers: [HealthController],
 })
 export class AppModule {}
