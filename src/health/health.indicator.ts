69aa62fe012f1cacebf532fbb704e35f2edac460
Gustavo Silva Quieregato <57011784+L3onT3chh@users.noreply.github.com>
2022-03-15 17:54:09 -0300

    Revert "Feat/173/create helthcheck"

--- src/health/health.indicator.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { HealthIndicator, HealthIndicatorResult } from '@nestjs/terminus';
-
-@Injectable()
-export class ApiHealthIndicator extends HealthIndicator {
-  public pingCheck(key: string): Promise<HealthIndicatorResult> {
-    return Promise.resolve(this.getStatus(key, true));
-  }
-}
