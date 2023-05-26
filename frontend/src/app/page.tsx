ecff89a5852a8a19f6888251bd826cb47d5de8d9
Jecé Xavier <jece@alunos.utfpr.edu.br>
2023-05-25 23:59:38 -0300

    style: updating code style

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-primary-100 flex items-center justify-center">
-      <h1 className="text-6xl font-bold underline text-primary-900">Hello World - LAMIA</h1>
-    </div>
-  );
+	return (
+		<div className="h-screen bg-primary-100 flex items-center justify-center">
+			<h1 className="text-6xl font-bold underline text-primary-900">
+				Hello World - LAMIA
+			</h1>
+		</div>
+	);
 }
