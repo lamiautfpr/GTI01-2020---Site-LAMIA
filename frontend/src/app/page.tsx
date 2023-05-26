95c501d5a0f2f69012918ee64dbdb738426d75be
Jecé Xavier <jece@alunos.utfpr.edu.br>
2023-05-26 02:55:49 +0000

    feat: config tailwind

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-	return <h1>Hello World - LAMIA</h1>;
+  return (
+    <div className="h-screen bg-primary-100 flex items-center justify-center">
+      <h1 className="text-6xl font-bold underline text-primary-900">Hello World - LAMIA</h1>
+    </div>
+  );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+       primary: {
+        100: '#CCE1EB',
+        200: '#B3D1E1',
+        300: '#99C2D7',
+        400: '#80B3CC',
+        500: '#66A4C2',
+        600: '#4D95B8',
+        700: '#3385AE',
+        800: '#1A76A4',
+        900: '#00679A',
+       }
+      }
     },
   },
   plugins: [],
