69f51b2d73fd5a027dc6ecee9fffa62ce70f806d
Jecé Xavier <jece@alunos.utfpr.edu.br>
2023-05-31 22:43:18 +0000

    feat: add cover

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,22 @@
-export default function Home() {
+const Cover = () => {
 	return (
-		<div className="h-screen bg-primary-100 flex items-center justify-center">
-			<h1 className="text-6xl font-bold underline text-primary-900">
-				Hello World - LAMIA
-			</h1>
+		<div className="h-screen flex items-center flex-col justify-center p-1">
+			<h1 className="text-center text-7xl font-bold text-primary-900 mb-4">LAMIA</h1>
+			<h3 className="text-center text-2xl  font-bold text-primary-900 mb-1">LABORATÓRIO DE APRENDIZADO DE MÁQUINA E IMAGENS APLICADOS À INDÚSTRIA</h3>
+			<p className="text-center text-xl font-normal text-secondary-900">UTFPR Campus Santa Helena</p>
 		</div>
 	);
 }
+
+const Home = () => {
+	return (
+		<>
+		<Cover/>
+		<main className="bg-primary-100 flex flex-col items-center justify-center">
+			
+		</main>
+		</>
+	);
+}
+
+export default Home;
